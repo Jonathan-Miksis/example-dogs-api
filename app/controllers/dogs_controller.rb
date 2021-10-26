2b130c93e4364ed5947a8af8b0825116f5ad4be8
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-25 20:57:02 -0400

    add authorization for creating dogs action, user must be logged in

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,13 +1,17 @@
 class DogsController < ApplicationController
 
   def create
-    dog = Dog.new(
-      name: params[:name],
-      age: params[:age],
-      breed: params[:breed]
-    )
-    dog.save
-    render json: dog.as_json
+    if current_user
+      dog = Dog.new(
+        name: params[:name],
+        age: params[:age],
+        breed: params[:breed]
+      )
+      dog.save
+      render json: dog.as_json
+    else
+      render json: {message: "You must be logged in to create new dogs"}
+    end
   end
 
 end
